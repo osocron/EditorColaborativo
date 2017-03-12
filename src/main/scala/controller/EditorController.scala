59a11e53e7a059006e4043e1340ba6b6dbdc4e76
osocron <osocron@gmail.com>
2017-03-12 15:58:47 -0600

    Seleccion de Interfaz implementada

--- src/main/scala/controller/EditorController.scala
+++ src/main/scala/controller/EditorController.scala
@@ -1,14 +1,18 @@
 package controller
 
-import java.net.URL
+import java.net.{NetworkInterface, URL}
+import java.util
 import java.util.ResourceBundle
-import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.collections.FXCollections
 import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.TextArea
+import javafx.scene.control.{ChoiceDialog, TextArea}
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import com.jfoenix.controls.JFXListView
+
+import scalafx.scene.control.Alert
+import scalafx.scene.control.Alert.AlertType
+
 
 
 class EditorController extends Initializable {
@@ -20,18 +24,45 @@ class EditorController extends Initializable {
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     println("Controller Intizialized")
-    val supervisor = createActorSystem()
+    chooseInterfaceAndStartSystem()
   }
 
 
-  def createActorSystem(): ActorRef = {
+  def createActorSystem(iface: String): ActorRef = {
     val system = ActorSystem("EditorSystem")
     system.actorOf(Props(
-      new Supervisor("wlp9s0",
+      new Supervisor(iface,
         textArea,
         listView,
         FXCollections.observableArrayList[Host]())).withDispatcher("akka.javafx-dispatcher"),
       name = "Supervisor")
+  }
+
+  def chooseInterfaceDialog(): Option[String] = {
+    val enum = NetworkInterface.getNetworkInterfaces
+    val choices = new util.ArrayList[String]()
+    while (enum.hasMoreElements) choices.add(enum.nextElement().getName)
+    val dialog = new ChoiceDialog[String]("Seleccione una opcion", choices)
+    dialog.setTitle("Seleccion de Interfaces")
+    dialog.setHeaderText("Favor de seleccionar la interfaz conectada a internet.")
+    dialog.setContentText("Interfaces: ")
+    val result = dialog.showAndWait()
+    if (result.isPresent) Some(result.get())
+    else None
+  }
+
+  def chooseInterfaceAndStartSystem(): Unit = {
+    val interface = chooseInterfaceDialog()
+    interface match {
+      case Some(iface) => createActorSystem(iface)
+      case None =>
+        val alert = new Alert(AlertType.Error)
+        alert.setTitle("Error")
+        alert.setHeaderText(null)
+        alert.setContentText("Es necesario seleccionar una opcion!")
+        alert.showAndWait()
+        chooseInterfaceAndStartSystem()
+    }
   }
 
 
